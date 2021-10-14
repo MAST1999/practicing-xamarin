e1162c693005db18f5349b359f71183d2cb0212d
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2021-10-14 13:02:10 +0330

    Basic Travel app, started to add database

---
+++ TravelRecordApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+    public Thickness MainPadding = new Thickness(2);
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            bool errors = false;
+
+            if (string.IsNullOrEmpty(EmailEntry.Text))
+            {
+                errors = true;
+                EmailLabel.Text = "Please enter your email address";
+            } else
+            {
+                EmailLabel.Text = "";
+            }
+
+            if (string.IsNullOrEmpty(PasswordEntry.Text))
+            {
+                errors = true;
+                PasswordLabel.Text = "Please enter your password";
+            } else
+            {
+                PasswordLabel.Text = "";
+            }
+
+            if (errors) return;
+
+            Navigation.PushAsync(new HomePage());
+        }
+    }
+}
---
+++ TravelRecordApp/NewTravelPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewTravelPage : ContentPage
+    {
+        public NewTravelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post
+            {
+                Experience = experianceEntry.Text,
+            };
+
+            SQLiteConnection connection = new SQLiteConnection(App.DatabaseLocation);
+            connection.CreateTable<Post>();
+
+            int rows = connection.Insert(post);
+            if (rows > 0)
+            {
+                DisplayAlert("Success", "Experiance successfully inserted", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Failure", "Failed to save the experiance", "Ok");
+            }
+            connection.Close();
+        }
+    }
+}
